2aeb5cebaa051587cbebdbdfbfdbf5c3ec94d9a5
rjoyadet <rjoyadet@gmail.com>
2023-09-17 15:39:09 -0400

    loop

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var names1 = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
 YaakovspeakWord.sayHello();
 PaulspeakWord.sayHello();
 FrankspeakWord.sayHello();
@@ -7,3 +9,7 @@ LauraspeakWord.sayHello();
 johnspeakWord.sayBye();
 jamesspeakWord.sayBye();
 jimspeakWord.sayBye();
+
+for (var i = 0; i < names1.length; i++) {
+  console.log("hello " + names1[i])
+};

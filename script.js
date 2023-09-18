ab3d8f0c8acbce3a2dbc024dc3f21fa27edc5b05
rjoyadet <rjoyadet@gmail.com>
2023-09-17 22:09:44 -0400

    speak

--- script.js
+++ script.js
@@ -12,21 +12,22 @@ jimspeakWord.sayBye();
 var names1 = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names1.length; i++) {
+
   console.log("hello " + names1[i]);
 
     var firstLetter = names1[i].charAt(0).toLowerCase();
-
-
-  if (firstLetter === 'j') {
+    if (firstLetter === 'j') {
       var byeSpeaker = "bye ";
-    byeSpeaker.speak=function(names1){
-      console.log(speakWord + names[i] + names);
+    byeSpeaker.speak = function(){
+      console.log("bye " + names1[i]);
     }
-   } else {
-     var helloSpeaker = "hello ";
-    helloSpeaker.speak=function(){
-      console.log(speakWord + names[i] + names);
+    } else {
+       var helloSpeaker = "hello ";
+    helloSpeaker.speak = function(names1){
+      console.log("hello " + names1[i]);
     }
   }
+
+
 }
 }) (window);
